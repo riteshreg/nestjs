9fee019a35ba80c713cf1d6e17fbdd7d45e83b9c
riteshreg <riteshregmi85@gmail.com>
2024-03-24 09:41:05 +0545

    added_user

--- overview_project/src/app.module.ts
+++ overview_project/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './modules/user/user.controller';
+import { UserService } from './modules/user/user.service';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
