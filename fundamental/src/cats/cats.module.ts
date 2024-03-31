49001a2e02c1ece185b1648d6878a602baae9767
riteshreg <riteshregmi85@gmail.com>
2024-03-31 08:13:29 +0545

    guide_added

---
+++ fundamental/src/cats/cats.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { CatsController } from './cats.controller';
+
+@Module({
+  providers: [CatsService,],
+  controllers:[CatsController]
+  
+})
+export class CatsModule {}
--- guide/src/app.module.ts
+++ guide/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
